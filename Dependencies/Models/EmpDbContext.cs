2ff02fe5d9e8c205d4c83ed9ce9fc0123e9bee5f
Laiba Razi <khanlayba72@gmail.com>
2023-10-02 01:10:56 +0500

    Files added

---
+++ Dependencies/Models/EmpDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Dependencies.Models
+{
+    public class EmpDbContext:DbContext
+    {
+
+        public EmpDbContext(DbContextOptions<EmpDbContext> options) : base(options)
+        { 
+        
+        }  
+        public DbSet<Emp> emps { get; set; }
+
+    }
+}
